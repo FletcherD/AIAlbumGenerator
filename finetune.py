1ba61f95653b0237103dde0f86cd2076e9ba79eb
Fletcher Dostie <fdostie@gmail.com>
2023-11-06 21:11:51 -0800

    finetune

--- finetune.py
+++ finetune.py
@@ -1,86 +1,109 @@
-"""
-Datafile is a text file with one sentence per line _DATASETS/data.txt
-tf_gpt2_keras_lora is the name of the fine-tuned model
-"""
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
 
-import tensorflow as tf
-from transformers import GPT2Tokenizer, TFGPT2LMHeadModel
-from transformers.modeling_tf_utils import get_initializer
-import os
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
 
-# use 2 cores
-tf.config.threading.set_intra_op_parallelism_threads(2)
-tf.config.threading.set_inter_op_parallelism_threads(2)
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
 
-# Use pretrained model if it exists
-# otherwise download it
-if os.path.exists("tf_gpt2_keras_lora"):
-    print("Model exists")
-    # use pretrained model
-    model = TFGPT2LMHeadModel.from_pretrained("tf_gpt2_keras_lora")
-else:
-    print("Downloading model")
-    model = TFGPT2LMHeadModel.from_pretrained("gpt2-large")
+from datasets import load_dataset
 
-# Load the tokenizer and model
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2-large")
+train_dataset = load_dataset('json', data_files='releases.jsonl', split='train')
 
-# Load and preprocess the data
-with open("dataset.txt", "r") as f:
-    lines = f.read().split("\n")
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig
 
-# Encode the data using the tokenizer and truncate the sequences to a maximum length of 1024 tokens
-input_ids_all = []
-for line in lines:
-    encoding = tokenizer.encode(line, add_special_tokens=True, max_length=1024, truncation=True)
-    input_ids_all.append(encoding)
+base_model_id = "llama/llama-2-7b/7B"
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16
+)
 
-# Define some params
-batch_size = 2
-num_epochs = 3
-learning_rate = 5e-5
+model = AutoModelForCausalLM.from_pretrained(base_model_id)
 
-# Define the optimizer and loss function
-optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+tokenizer = AutoTokenizer.from_pretrained(
+    base_model_id,
+    padding_side="left",
+    add_eos_token=True,
+    add_bos_token=True,
+)
+tokenizer.pad_token = tokenizer.eos_token
 
-# Fine-tune the model using low-rank adaptation and attention pruning
-for layer in model.transformer.h:
-    layer.attention_output_dense = tf.keras.layers.Dense(units=256, kernel_initializer=get_initializer(0.02), name="attention_output_dense")
-    
-model.summary()
+def generate_and_tokenize_prompt(prompt):
+    return tokenizer(prompt)
 
-# Train the model
-for epoch in range(num_epochs):
-    print(f"Epoch {epoch + 1}/{num_epochs}")
-    
-    # Shuffle the input data
-    input_ids_all = tf.random.shuffle(input_ids_all)
-    input_ids = input_ids_all[:65536]
-    
-    for i in range(0, len(input_ids), batch_size):
-        batch = input_ids[i:i+batch_size]
-        # Pad the batch to the same length
-        batch = tf.keras.preprocessing.sequence.pad_sequences(batch, padding="post")
-        # Define the inputs and targets
-        inputs = batch[:, :-1]
-        targets = batch[:, 1:]
-        # Compute the predictions and loss
-        with tf.GradientTape() as tape:
-            logits = model(inputs)[0]
-            loss = loss_fn(targets, logits)
-        # Compute the gradients and update the parameters
-        gradients = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-        
-        # Print the loss every 10 batches
-        if i % (10 * batch_size) == 0:
-            print(f"Batch {i}/{len(input_ids)} - loss: {loss:.4f}")
-            
-# Save the fine-tuned model
-model.save_pretrained("tf_gpt2_keras_lora")
+tokenized_train_dataset = train_dataset.map(generate_and_tokenize_prompt)
 
-# Generate text using the fine-tuned model
-input_ids = tokenizer.encode("How much wood", return_tensors="tf")
-output = model.generate([], max_length=100, do_sample=True, top_k=50, top_p=0.95, temperature=0.9)
-print(tokenizer.decode(output[0], skip_special_tokens=True))
+from peft import prepare_model_for_kbit_training
+
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+
+from peft import LoraConfig, get_peft_model
+
+config = LoraConfig(
+    r=32,
+    lora_alpha=64,
+    target_modules=[
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "o_proj",
+        "gate_proj",
+        "up_proj",
+        "down_proj",
+        "lm_head",
+    ],
+    bias="none",
+    lora_dropout=0.05,  # Conventional
+    task_type="CAUSAL_LM",
+)
+
+model = get_peft_model(model, config)
+print_trainable_parameters(model)
+
+# Apply the accelerator. You can comment this out to remove the accelerator.
+model = accelerator.prepare_model(model)
+
+import transformers
+from datetime import datetime
+
+project = "journal-finetune"
+base_model_name = "llama2-7b"
+run_name = base_model_name + "-" + project
+output_dir = "./" + run_name
+
+tokenizer.pad_token = tokenizer.eos_token
+
+trainer = transformers.Trainer(
+    model=model,
+    train_dataset=tokenized_train_dataset,
+    eval_dataset=tokenized_val_dataset,
+    args=transformers.TrainingArguments(
+        output_dir=output_dir,
+        warmup_steps=1,
+        per_device_train_batch_size=2,
+        gradient_accumulation_steps=1,
+        #max_steps=500,
+        learning_rate=2.5e-5, # Want a small lr for finetuning
+        bf16=True,
+        optim="paged_adamw_8bit",
+        logging_dir="./logs",        # Directory for storing logs
+        save_strategy="steps",       # Save the model checkpoint every logging step
+        save_steps=50,                # Save checkpoints every 50 steps
+        evaluation_strategy="steps", # Evaluate the model every logging step
+        eval_steps=50,               # Evaluate and save checkpoints every 50 steps
+        do_eval=True,                # Perform evaluation at the end of training
+        #report_to="wandb",           # Comment this out if you don't want to use weights & baises
+        run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}"          # Name of the W&B run (optional)
+    ),
+    data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
+)
+
+model.config.use_cache = False  # silence the warnings. Please re-enable for inference!
+trainer.train()
+
